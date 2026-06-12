2adf1383a1c56919950860023c6d3eb9dbffc749
Andres Haro <hello@aharo.dev>
2026-06-12 17:12:06 -0600

    Initial Commit

---
+++ navycare/navycareApp.swift
@@ -0,0 +1,17 @@
+//
+//  navycareApp.swift
+//  navycare
+//
+//  Created by Alan Haro on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct navycareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
